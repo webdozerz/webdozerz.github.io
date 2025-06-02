798f35766404afd95f2e0d1c0872f5131e331a81
iliyaandrushov <webdozerz@gmail.com>
2025-06-02 12:35:06 +0700

    Styles

---
+++ components/BaseResultCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="['result-card', bgClass]">
+    <p class="title">{{ title }}</p>
+    <p class="value">{{ value }} ч</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  title?: string
+  color?: string
+  value?: number
+}>()
+
+const bgClass = computed(() => {
+  switch (props.color) {
+    case 'green':
+      return 'green'
+    case 'red':
+      return 'red'
+    default:
+      return 'blue'
+  }
+})
+</script> 
